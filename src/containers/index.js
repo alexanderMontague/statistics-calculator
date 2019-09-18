bd72f3e34e1c52a7dfb0438cc36db5f855888ff8
alexanderMontague <xturnermonty@gmail.com>
2019-09-18 18:40:27 -0400

    add MAD, z score and mmm

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,23 +1,71 @@
 import React, { Component, Fragment } from "react";
+import { Paper, TextField, Button, Card, CardContent } from "@material-ui/core";
 import styles from "./styles.scss";
 
 import variance from "../assets/variance.png";
-import { sampleVariance } from "../helpers/calculations";
+import MAD from "../assets/MAD.png";
+import mean from "../assets/mean.png";
 
-import { Paper, TextField, Button, Card, CardContent } from "@material-ui/core";
+import {
+  sampleVariance,
+  meanAvgDeviation,
+  zScore,
+  meanMedianMode
+} from "../helpers/calculations";
 
 class Layout extends Component {
   state = {
-    inputText: "",
+    varianceObservations: "",
     varianceAnswer: "",
-    squareRootVariance: ""
+    squareRootVariance: "",
+
+    madObservations: "",
+    madAnswer: "",
+
+    zScoreVal: "",
+    zScoreObservations: "",
+    zScoreAnswer: "",
+
+    mmmObservations: "",
+    mmmVal: [], // 0: mean, 1: median, 2: mode,
+    mmmOutliers: ""
   };
 
   calculateHandler(calculation) {
-    const [varianceAnswer, squareRootVariance] = sampleVariance(
-      this.state.inputText.split(" ")
-    );
-    this.setState({ varianceAnswer, squareRootVariance });
+    switch (calculation) {
+      case "variance":
+        const [varianceAnswer, squareRootVariance] = sampleVariance(
+          this.state.varianceObservations.split(" ")
+        );
+        this.setState({ varianceAnswer, squareRootVariance });
+        break;
+
+      case "mad":
+        const madAnswer = meanAvgDeviation(
+          this.state.madObservations.split(" ")
+        );
+        this.setState({ madAnswer });
+        break;
+
+      case "zScore":
+        const zScoreAnswer = zScore(
+          this.state.zScoreVal,
+          this.state.zScoreObservations.split(" ")
+        );
+        this.setState({ zScoreAnswer });
+        break;
+
+      case "mmm":
+        const mmmVal = meanMedianMode(
+          this.state.mmmObservations.split(" "),
+          this.state.mmmOutliers.split(" ")
+        );
+        this.setState({ mmmVal });
+        break;
+
+      default:
+        break;
+    }
   }
 
   render() {
@@ -25,6 +73,7 @@ class Layout extends Component {
       <Fragment>
         <h2 className={styles.header}>STAT*2040 Calculator</h2>
 
+        {/* Variance */}
         <Paper className={styles.calculation}>
           <h4 className={styles.calcHeader}>Sample Variance</h4>
           <div className={styles.formulaContainer}>
@@ -39,7 +88,9 @@ class Layout extends Component {
                 multiline
                 className={styles.textField}
                 margin="normal"
-                onChange={e => this.setState({ inputText: e.target.value })}
+                onChange={e =>
+                  this.setState({ varianceObservations: e.target.value })
+                }
               />
             </div>
           </div>
@@ -71,6 +122,150 @@ class Layout extends Component {
               variant="contained"
               color="primary"
               onClick={() => this.calculateHandler("variance")}
+            >
+              Calculate Variance
+            </Button>
+          </div>
+        </Paper>
+
+        {/* Mean Absolute Deviation */}
+        <Paper className={styles.calculation}>
+          <h4 className={styles.calcHeader}>Mean Absolute Deviation (MAD)</h4>
+          <div className={styles.formulaContainer}>
+            <div className={styles.formulaBox}>
+              <b>Formula:</b>
+              <img className={styles.formulaPic} src={MAD} />
+            </div>
+            <div className={styles.inputBox}>
+              <TextField
+                id="standard-textarea"
+                label={`Enter all observations (separated by spaces)`}
+                multiline
+                className={styles.textField}
+                margin="normal"
+                onChange={e =>
+                  this.setState({ madObservations: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className={styles.submitBox}>
+            <Card className={styles.card}>
+              <CardContent>
+                Answer:
+                <br />
+                <br />
+                {this.state.madAnswer}
+                {this.state.madAnswer.length === 0 ? "" : " units"}
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.calculateHandler("mad")}
+            >
+              Calculate Mean Absolute Deviation
+            </Button>
+          </div>
+        </Paper>
+
+        {/* z-score */}
+        <Paper className={styles.calculation}>
+          <h4 className={styles.calcHeader}>z-score</h4>
+          <div className={styles.formulaContainer}>
+            <div className={styles.formulaBox}>
+              <b>Formula:</b>
+              <img className={styles.formulaPic} src={MAD} />
+            </div>
+            <div className={styles.inputBox}>
+              <TextField
+                id="standard-textarea"
+                label={`Enter ith observation value`}
+                multiline
+                className={styles.textField}
+                margin="normal"
+                onChange={e => this.setState({ zScoreVal: e.target.value })}
+              />
+              <TextField
+                id="standard-textarea"
+                label={`Enter all observations`}
+                multiline
+                className={styles.textField}
+                margin="normal"
+                onChange={e =>
+                  this.setState({ zScoreObservations: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className={styles.submitBox}>
+            <Card className={styles.card}>
+              <CardContent>
+                Answer:
+                <br />
+                <br />
+                {this.state.zScoreAnswer}
+                {this.state.zScoreAnswer.length === 0 ? "" : " units"}
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.calculateHandler("zScore")}
+            >
+              Calculate Mean Absolute Deviation
+            </Button>
+          </div>
+        </Paper>
+
+        {/* Mean, Median, Mode */}
+        <Paper className={styles.calculation}>
+          <h4 className={styles.calcHeader}>Mean Median and Mode</h4>
+          <div className={styles.formulaContainer}>
+            <div className={styles.formulaBox}>
+              <b>Formula:</b>
+              <img className={styles.formulaPic} src={mean} />
+            </div>
+            <div className={styles.inputBox}>
+              <TextField
+                id="standard-textarea"
+                label={`Enter all observations (separated by spaces)`}
+                multiline
+                className={styles.textField}
+                margin="normal"
+                onChange={e =>
+                  this.setState({ mmmObservations: e.target.value })
+                }
+              />
+              <TextField
+                id="standard-textarea"
+                label={`Outlier(s) to be removed (separated by spaces)`}
+                multiline
+                className={styles.textField}
+                margin="normal"
+                onChange={e => this.setState({ mmmOutliers: e.target.value })}
+              />
+            </div>
+          </div>
+          <div className={styles.submitBox}>
+            <Card className={styles.card}>
+              <CardContent>
+                Mean: {this.state.mmmVal[0]}
+                <br />
+                <br />
+                Median: {this.state.mmmVal[1]}
+                <br />
+                <br />
+                Mode(s):{" "}
+                {this.state.mmmVal[2] && this.state.mmmVal[2].join(" ")}
+                <br />
+                <br />
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.calculateHandler("mmm")}
             >
               Calculate Variance
             </Button>
--- src/helpers/calculations.js
+++ src/helpers/calculations.js
@@ -1,7 +1,15 @@
-export const sampleVariance = observations => {
-  observations = observations.filter(num => !isNaN(num) && num.length !== 0);
-  const length = observations.length;
-  const mean = observations.reduce((acc, num) => acc + Number(num), 0) / length;
+const setUpCalculations = observations => {
+  const parsedObservations = observations.filter(
+    num => !isNaN(num) && num.length !== 0
+  );
+  const length = parsedObservations.length;
+  const mean =
+    parsedObservations.reduce((acc, num) => acc + Number(num), 0) / length;
+  return [parsedObservations, length, mean];
+};
+
+export const sampleVariance = rawObservations => {
+  const [observations, length, mean] = setUpCalculations(rawObservations);
 
   const variance =
     observations.reduce(
@@ -11,4 +19,69 @@ export const sampleVariance = observations => {
     (length - 1);
 
   return [variance, Math.sqrt(variance)];
+};
+
+export const meanAvgDeviation = rawObservations => {
+  const [observations, length, mean] = setUpCalculations(rawObservations);
+
+  const MAD =
+    observations.reduce((acc, num) => acc + Math.abs(num - mean), 0) / length;
+
+  return MAD;
+};
+
+export const zScore = (observationVal, rawObservations) => {
+  const [observations, noop1, mean] = setUpCalculations(rawObservations);
+  const [noop2, standardDeviation] = sampleVariance(observations);
+
+  return (observationVal - mean) / standardDeviation;
+};
+
+export const meanMedianMode = (rawObservations, outliers) => {
+  outliers = outliers.filter(num => !isNaN(num) && num.length !== 0);
+  console.log(outliers, 1);
+  const [observations, length, mean] = setUpCalculations(rawObservations);
+
+  let sortedObservations = observations.sort((a, b) => a < b);
+  sortedObservations = sortedObservations.filter(
+    num => !outliers.includes(num)
+  );
+
+  const median =
+    length % 2 !== 0
+      ? sortedObservations[Math.floor(length / 2)]
+      : (Number(sortedObservations[length / 2 - 1]) +
+          Number(sortedObservations[length / 2])) /
+        2;
+
+  const modeMap = {};
+  let modeArr = [];
+  let currMax = 0;
+
+  // tally all occurances
+  for (let i = 0; i < length; i++) {
+    modeMap[observations[i]] = !modeMap[observations[i]]
+      ? 1
+      : modeMap[observations[i]] + 1;
+
+    // keep track of highest occurance in case of multiple modes
+    if (modeMap[observations[i]] > currMax) {
+      currMax = modeMap[observations[i]];
+    }
+  }
+
+  // grab modes
+  for (let num in modeMap) {
+    if (modeMap[num] === currMax) {
+      modeArr.push(num);
+    }
+  }
+
+  if (currMax === 1) {
+    modeArr = ["No Mode"];
+  }
+
+  console.log(mean, median, modeArr);
+
+  return [mean, median, modeArr];
 };
